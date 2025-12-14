3b7c1a2874f9db6764a0c0b0850cdbaf03c6de49
Eugene <48792577+Malomnogo@users.noreply.github.com>
2025-12-15 01:52:50 +0300

    feat: code-quality (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
+    id("dev.detekt") version ("2.0.0-alpha.1")
 }

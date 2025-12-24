dcb7e235a04f4026598e37b167e139c2184824fc
Eugene <auricamercer@gmail.com>
2025-12-24 19:01:12 +0300

    feat presentation mapper test

---
+++ feature/list/presentation/src/test/java/com/malomnogo/lsit/presentation/PokemonItemMapperTest.kt
@@ -0,0 +1,30 @@
+package com.malomnogo.lsit.presentation
+
+import com.malomnogo.domain.PokemonDomain
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class PokemonItemMapperTest {
+
+    private val generateImage = FakeGenerateImageUrl()
+    private val generateNumber = FakeGenerateNumber()
+    private val mapper = PokemonItemMapper.Base(generateImage, generateNumber)
+
+    @Test
+    fun `map domain model to ui model correctly`() {
+        val domainModel = PokemonDomain(
+            id = 25,
+            name = "Pikachu"
+        )
+
+        val uiModel = mapper.map(domainModel)
+
+        val expected = PokemonUiItem(
+            id = 25,
+            name = "Pikachu",
+            number = "25",
+            imageUrl = "https://25.jpg"
+        )
+        assertEquals(expected, uiModel)
+    }
+}
